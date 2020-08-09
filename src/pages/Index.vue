5fb0a753c7b846d1d183b788401c4df93d2776e5
algorithm <odjaidri@gmail.com>
2020-08-10 00:04:01 +0100

    add courses section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -401,6 +401,25 @@
           </div>
         </div>
       </div>
+      <!-- courses section -->
+      <div id="courses" class="courses-section my-24 text-center">
+        <h3 class="mb-6 text-3xl font-bold">I create videos on <g-link to="/">YouTube</g-link></h3>
+        <p class="max-w-xl mx-auto">
+          Over the last year, I've created courses on VS Code, Web Development, Pytohn, Node.js, and more. I love being able to put the things I've learned into a package for others to learn from.
+        </p>
+        <div class="mt-6 max-w-xl mx-auto">
+          <h3 class="text-2xl mb-6 font-bold">
+            Looking for Custom Content?
+          </h3>
+          <p>
+            I often get requests to create custom content like individual courses, one-off videos, articles, etc. If you're interested in something,contact me and include all of the necessary details.
+          </p>
+          <g-link
+            class="btn mr-auto mt-8 font-bold py-3 px-8 rounded-sm text-sm inline-block uppercase"
+            v-scroll-to="'#contact'"
+          >contact</g-link>
+        </div>
+      </div>
     </div>
   </Layout>
 </template>
@@ -650,5 +669,12 @@ export default {
   font-weight: bold;
 }
 
+/* courses section */
+
+.courses-section h3 a {
+  color: var(--special-color);
+  text-decoration: underline;
+  cursor: pointer;
+}
 
 </style>
